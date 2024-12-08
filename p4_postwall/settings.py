b65070755153a6e3d57fba70328fa8a81006555e
GlennJohansson85 <g.j@live.se>
2024-12-08 20:30:23 +0100

    Update settings.py

--- p4_postwall/settings.py
+++ p4_postwall/settings.py
@@ -1,31 +1,44 @@
-# settings.py
 import os
 from pathlib import Path
 import dj_database_url
+
 
 # Load environment variables from env.py
 if os.path.isfile('env.py'):
     import env
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Django
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-DEBUG = True
 
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+
+
+# Dynamically get Heroku app URL from environment
+HEROKU_APP_URL = os.environ.get('HEROKU_APP_URL', 'p4postwall.herokuapp.com')
+
+
+# Dynamically configure allowed hosts & CSRF trusted origins
 ALLOWED_HOSTS = [
-    'p4postwall-4a255b5600ea.herokuapp.com',
+    HEROKU_APP_URL,
     'localhost',
     '127.0.0.1'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://p4postwall-4a255b5600ea.herokuapp.com'
+    HEROKU_APP_URL
 ]
 
+
+# Auth
 AUTH_USER_MODEL = 'accounts.Profile'
 
+
+# Installed Apps
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +52,8 @@ INSTALLED_APPS = [
     'gunicorn',
 ]
 
+
+# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -55,15 +70,14 @@ ROOT_URLCONF = 'p4_postwall.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.template.context_processors.csrf',
-                'django.template.context_processors.tz',
-                'django.template.context_processors.static',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
@@ -78,6 +92,8 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
+
+# Database Configuration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -86,34 +102,28 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# Intern.
+# General settings.
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Email Conf.
+
+# Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 
 # Local Static & Media Configuraiton
 STATIC_URL = '/static/'
@@ -123,6 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 # Deployed Static & Media Configuration
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'p4postwall'
@@ -131,16 +142,16 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY   = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
     # Dynamic custom domain
-    AWS_S3_CUSTOM_DOMAIN    = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 
     # Explicit location configuration for static and media
-    STATICFILES_LOCATION    = 'static'
-    MEDIAFILES_LOCATION     = 'media'
+    STATICFILES_LOCATION = 'static'
+    MEDIAFILES_LOCATION = 'media'
 
     # Storage classes
-    STATICFILES_STORAGE     = 'custom_storages.StaticStorage'
-    DEFAULT_FILE_STORAGE    = 'custom_storages.MediaStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
     # URLs for static and media files
-    STATIC_URL              = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL               = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
